8f28ad1255e576d611d1846737d92cc6e6fc18c3
coresynapses <charlie.suarez273@protonmail.com>
2024-03-15 03:49:08 -0500

    The project compiles and runs successfully

---
+++ oogwayserver/src/main/java/xyz/coresynapses/oogwayserver/OogwayserverApplication.java
@@ -0,0 +1,13 @@
+package xyz.coresynapses.oogwayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OogwayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OogwayserverApplication.class, args);
+	}
+
+}
--- src/main/java/xyz/coresynapses/oogwayserver/OogwayserverApplication.java
+++ src/main/java/xyz/coresynapses/oogwayserver/OogwayserverApplication.java
@@ -2,12 +2,21 @@ package xyz.coresynapses.oogwayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OogwayserverApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(OogwayserverApplication.class, args);
-	}
-
+    
+    public static void main(String[] args) {
+	SpringApplication.run(OogwayserverApplication.class, args);
+    }
+    
+    
+    @GetMapping("/oogway")
+    public String oogway(@RequestParam(value = "name", defaultValue = "Doctor Who") String name) {
+	return String.format("Welcome to Oogway, %s.", name);
+    }
 }
