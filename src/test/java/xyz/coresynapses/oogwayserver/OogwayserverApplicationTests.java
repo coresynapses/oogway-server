5e35ca02db2321fb0a37ff3a32287d70459b4f9e
coresynapses <charlie.suarez273@protonmail.com>
2024-03-15 03:22:43 -0500

    Turns out I have to use Spring's project template. kek

---
+++ src/test/java/xyz/coresynapses/oogwayserver/OogwayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.coresynapses.oogwayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OogwayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
